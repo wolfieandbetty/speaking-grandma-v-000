1d19e85f03b0419daaae17bfaa7903ed564d4fac
Cat Norris <cannenorris@yahoo.com>
2017-11-07 21:59:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,10 @@
 def speak_to_grandma(phrase)
   if phrase==phrase.upcase && phrase=="I LOVE YOU GRANDMA!"then
     puts "I LOVE YOU TOO PUMPKIN!"
-    
+
   elsif phrase==phrase.upcase then
   puts "NOT, NOT SINCE 1938!"
-  
+
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
