93139aeb36ee4314d0e85f3b80a7425458609499
Cat Norris <cannenorris@yahoo.com>
2017-11-07 22:04:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
   "I LOVE YOU TOO PUMPKIN!"
 
   elsif phrase==phrase.upcase then
-  "NOT, NOT SINCE 1938!"
+  "NO, NOT SINCE 1938!"
 
   else
   "HUH?! SPEAK UP, SONNY!"
